b95dd2d42d11aee890664f4ff008ff7e687fd318
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-06-14 02:34:06 +0100

    ch(app-structure)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Welcome to typescript in Node");
+// third-party libraries
+// import express from "express";
+// import http from "http";
+
+// const app = http.createServer();
+
+// app.listen(5000, () => console.log(`App running on port 5000`));
